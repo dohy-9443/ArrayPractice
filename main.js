44250ec53d33678be065c83b50ab82e4b62a1b1a
dohy-9443 <dong_hyun95@hanmail.net>
2021-09-21 19:50:04 +0900

    hover 진행중

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const ulEl = document.querySelector("#jsUl");
 // 객체의 key 개수
 
 let navMain = [];
+let liHeight = [];
 
 for (let i = 0; i < Object.keys(Arr).length; i++) {
   navMain.push(Object.keys(Arr)[i]);
@@ -28,7 +29,6 @@ lis.forEach((li) => {
 const liUlEls = document.querySelectorAll("#jsUl > li > ul");
 
 navMain.forEach((cur, index) => {
-  console.log(`${cur} 시작`);
   for (let i = 0; i < Arr[cur].length; i++) {
     const li = document.createElement("li");
     li.classList.add("flex");
@@ -36,13 +36,27 @@ navMain.forEach((cur, index) => {
     liUlEls[index].appendChild(li);
 
     li.innerText = Arr[cur][i];
-    console.log(`나는 ${i} 번째`);
-  }
 
-  console.log(`${cur} 끝`);
+    // console.log(`나는 ${i} 번째`);
+  }
 });
 
 liUlEls.forEach((ul) => {
-  console.log(ul);
+  let height = ul.childNodes.length * 30;
+  liHeight.push(height);
+});
+
+const maxHeight = Math.max(...liHeight);
+
+lis.forEach((li) => {
+  li.childNodes[1].style.height = maxHeight;
+
+  li.addEventListener("mouseover", function (event) {
+    this.childNodes[this.childNodes.length - 1].style.height = maxHeight;
+  });
+
+  li.addEventListener("mouseout", function () {
+    this.childNodes[this.childNodes.length - 1].style.height = 0;
+  });
 });
 // console.log(navMain);
